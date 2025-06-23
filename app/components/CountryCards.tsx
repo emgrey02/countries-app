4552e9fd548e61ebf468bf4bcfa3a998c20587c7
emgrey02 <emgrey02@gmail.com>
2025-06-23 12:15:10 -0400

    Fix fetch call

--- app/components/CountryCards.tsx
+++ app/components/CountryCards.tsx
@@ -1,29 +1,37 @@
-import { Card } from "./Card";
-import { SkeletonCard } from "./SkeletonCard";
-import { Country } from "../types";
+import { Card } from './Card';
+import { SkeletonCard } from './SkeletonCard';
+import { Country } from '../types';
 
-export const CountryCards = ({ countries, isLoading = false }: { countries: Country[], isLoading?: boolean }) => {
-    const gridClasses = 'grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8';
+export const CountryCards = ({
+	countries,
+	isLoading = false,
+}: {
+	countries: Country[];
+	isLoading?: boolean;
+}) => {
+	const gridClasses =
+		'grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8';
+	console.log(countries);
 
-    if (isLoading) {
-        return (
-            <ul className={gridClasses}>
-                {[...Array(8)].map((_, index) => (
-                    <li key={index}>
-                        <SkeletonCard />
-                    </li>
-                ))}
-            </ul>
-        );
-    }
+	if (isLoading) {
+		return (
+			<ul className={gridClasses}>
+				{[...Array(8)].map((_, index) => (
+					<li key={index}>
+						<SkeletonCard />
+					</li>
+				))}
+			</ul>
+		);
+	}
 
-    return (
-        <ul className={gridClasses}>
-            {countries?.map((country: Country) => (
-               <li key={country.name.common}>
-                    <Card country={country}/>
-               </li>
-            ))}
-        </ul>
-    );
+	return (
+		<ul className={gridClasses}>
+			{countries?.map((country: Country) => (
+				<li key={country.name.common}>
+					<Card country={country} />
+				</li>
+			))}
+		</ul>
+	);
 };
